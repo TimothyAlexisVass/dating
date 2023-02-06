6bdfc598eb22b2391089206f2c9a9597c058dab9
Timothy Alexis Vass <timvas@bredband2.se>
2023-02-06 18:41:47 +0100

    Add user sign in and sessions functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,43 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
+  def signin
+  end
+
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    if params[:session][:username_or_email]
+      username_or_email = params[:session][:username_or_email].downcase
+      user = if username_or_email.include?("@")
+        User.find_by(email: username_or_email)
+      else
+        User.find_by(username: username_or_email)
+      end
+    else
+      User.find_by(email: params[:session][:email])
+    end
     if user && user.authenticate(params[:session][:password])
-      log_in user
+      sign_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      render 'sign_in'
     end
   end
 
   def destroy
-    log_out
+    sign_out
     redirect_to root_url
   end
+
+  private
+  
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def sign_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
 end
