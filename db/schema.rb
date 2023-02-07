8b90861bb2aae7dfeb6fe8a8b8707cef93b5d1df
Timothy Alexis Vass <timvas@bredband2.se>
2023-02-07 20:24:46 +0100

    Rename verified to verified_user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_174345) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_192344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -129,7 +129,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_174345) do
     t.float "longitude"
     t.float "latitude"
     t.date "last_active", default: -> { "CURRENT_TIMESTAMP" }
-    t.boolean "verified", default: false
+    t.boolean "verified_user", default: false
     t.boolean "verified_congregation", default: false
     t.boolean "verified_rebirth", default: false
     t.boolean "is_active", default: false
